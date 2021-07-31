bb34e14d98ab495fa6e6fad00dc07139835c3678
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2021-08-01 01:28:12 +0530

    add template files

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import ProTip from "./ProTip";
 
 const Copyright = () => {
     return (
@@ -27,7 +26,6 @@ const App = () => {
                 <Typography variant="h4" component="h1" gutterBottom>
                     Create React App v4-beta example with TypeScript
                 </Typography>
-                <ProTip/>
                 <div>
                     <Copyright/>
                 </div>
